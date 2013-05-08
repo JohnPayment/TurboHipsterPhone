102b1ef77876db4dd3d0e015a71a53d2f6df7bf6
unknown <john0payment@gmail.com>
2013-05-08 11:27:42 -0700

    FIxed formatting

--- Code/Button3/src/Button3.java
+++ Code/Button3/src/Button3.java
@@ -6,15 +6,13 @@ import Util.Strings;
 import javax.microedition.lcdui.Display;
 import javax.microedition.midlet.MIDlet;
 
-
 /**
  * Application for button3. Requests server to end monitoring for this specific
  * user.
  *
  * @author Team Cirno
  */
-public class Button3
-        extends MIDlet
+public class Button3 extends MIDlet
 {
     /**
      * Constant string representing the connection being OK.
@@ -30,15 +28,16 @@ public class Button3
         Popup p;
         Display disp;
         disp = Display.getDisplay(this);
-        try{
-        
+        try
+		{
             String[] str = Strings.getAllMessages();
             p = new Popup(this, str[Messages.SENDING], false, null);
             disp.setCurrent(p);
             response = Sender.send(null, NetworkRequest.END_MONITORING_MESSAGE);
             p = new Popup(this, (response.equals(OK)) ? str[Messages.RECEIVED] : response, true, null);
             disp.setCurrent(p);
-        }catch(Exception e){
+        } catch(Exception e)
+		{
             //most likely to happen if there is an error in Strings.getAllMessages()
             Popup pop = new Popup(this, "Please run configurator first.", true, null);
             disp.setCurrent(pop);
@@ -52,5 +51,4 @@ public class Button3
     public void destroyApp(boolean unconditional)
     {
     }
-
 }
