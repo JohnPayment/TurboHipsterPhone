690b0d3754c76b44338f3880041aac8b747af08f
unknown <john0payment@gmail.com>
2013-05-18 15:32:25 -0700

    Formatted code and added headers

--- Code/Button3/src/Button3.java
+++ Code/Button3/src/Button3.java
@@ -6,49 +6,99 @@ import Util.Strings;
 import javax.microedition.lcdui.Display;
 import javax.microedition.midlet.MIDlet;
 
-/**
- * Application for button3. Requests server to end monitoring for this specific
- * user.
+/*******************************************************************************
+ * CLASS: Button2
  *
- * @author Team Cirno
- */
+ * FUNCTIONS: void startApp()
+ *            void pauseApp()
+ *            void destroyApp(boolean unconditional)
+ *
+ * DATE: 2013-01-18
+ *
+ * REVISIONS: 2013-05-18
+ *            Steve Lo
+ *            John Payment
+ *
+ * DESIGNER: Team Cirno
+ *
+ * PROGRAMMER: Team Cirno
+ *
+ * NOTES: This is a Java Mobile App intended to connect to the validation server
+ *        with pre-stored credentials. It then reports that the device should
+ *        no longer be monitored by the server.
+ *******************************************************************************/
 public class Button3 extends MIDlet
 {
-    /**
-     * Constant string representing the connection being OK.
-     */
-    private static final String OK = "OK"; 
-    
-    /**
-     * When application is launched, startApp will be invoked.
-     */
-    public void startApp()
-    {
-        String response;
-        Popup p;
-        Display disp;
-        disp = Display.getDisplay(this);
-        try
-		{
-            String[] str = Strings.getAllMessages();
-            p = new Popup(this, str[Messages.SENDING], false, null);
-            disp.setCurrent(p);
-            response = Sender.send(null, NetworkRequest.END_MONITORING_MESSAGE);
-            p = new Popup(this, (response.equals(OK)) ? str[Messages.RECEIVED] : response, true, null);
-            disp.setCurrent(p);
-        } catch(Exception e)
-		{
-            //most likely to happen if there is an error in Strings.getAllMessages()
-            Popup pop = new Popup(this, "Please login using the Configurator first", true, null);
-            disp.setCurrent(pop);
-        }
-    }
+	/**
+	 * Constant string representing the connection being OK.
+	 */
+	private static final String OK = "OK"; 
 
-    public void pauseApp()
-    {
-    }
+	/*************************************************************************** 
+	 * FUNCTION: startApp
+	 * 
+	 * DATE: 2013-01-30
+	 * 
+	 * REVISIONS: 2013-05-10
+	 *            Steve Lo
+	 * 
+	 * DESIGNER: Team Cirno
+	 * 
+	 * PROGRAMMER: Team Cirno
+	 * 
+	 * INTERFACE: void startApp()
+	 * 
+	 * NOTES: Called when the app is launched.
+	 ***************************************************************************/
+	public void startApp()
+	{
+		String response;
+		Popup p;
+		Display disp;
+		disp = Display.getDisplay(this);
+		try
+		{
+			String[] str = Strings.getAllMessages();
+			p = new Popup(this, str[Messages.SENDING], false, null);
+			disp.setCurrent(p);
+			response = Sender.send(null, NetworkRequest.END_MONITORING_MESSAGE);
+			p = new Popup(this, (response.equals(OK)) ? str[Messages.RECEIVED] : response, true, null);
+			disp.setCurrent(p);
+		} catch(Exception e)
+		{
+			//most likely to happen if there is an error in Strings.getAllMessages()
+			Popup pop = new Popup(this, "Please login using the Configurator first", true, null);
+			disp.setCurrent(pop);
+		}
+	}
 
-    public void destroyApp(boolean unconditional)
-    {
-    }
+	/*************************************************************************** 
+	 * FUNCTION: pauseApp
+	 * 
+	 * DESIGNER: N/A
+	 * 
+	 * PROGRAMMER: N/A
+	 * 
+	 * INTERFACE: void pauseApp()
+	 * 
+	 * NOTES: Default function inherited by the parent class.
+	 ***************************************************************************/
+	public void pauseApp()
+	{
+	}
+
+	/*************************************************************************** 
+	 * FUNCTION: destroyApp
+	 * 
+	 * DESIGNER: N/A
+	 * 
+	 * PROGRAMMER: N/A
+	 * 
+	 * INTERFACE: void destroyApp(boolean unconditional)
+	 * 
+	 * NOTES: Default function inherited by the parent class.
+	 ***************************************************************************/
+	public void destroyApp(boolean unconditional)
+	{
+	}
 }
